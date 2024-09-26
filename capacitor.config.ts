4e96c6006b623f5d6c9b881c25b0a389303a9b15
supercandela <supercandela@gmail.com>
2024-09-26 15:27:52 -0300

    agrego esqueleto de app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.ioniccapacitorapp.Captura&Vota',
-  appName: 'Captura&Vota',
+  appId: 'com.ioniccapacitorapp.AprendeConUnToque',
+  appName: 'AprendeConUnToque',
   webDir: 'www'
 };
 
